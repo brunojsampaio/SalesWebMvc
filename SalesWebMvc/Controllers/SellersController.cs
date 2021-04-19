65bb41133c894ce8be2dca5f52f09da09b88a68d
Bruno Sampaio <brunojsampaio@gmail.com>
2021-04-19 07:54:08 -0300

    Delete seller

--- SalesWebMvc/Controllers/SellersController.cs
+++ SalesWebMvc/Controllers/SellersController.cs
@@ -40,5 +40,29 @@ namespace SalesWebMvc.Controllers
             _sellersService.Insert(seller);
             return RedirectToAction(nameof(Index));
         }
+
+        public IActionResult Delete(int? id)
+        {
+            if (id ==  null)
+            {
+                return NotFound();
+            }
+
+            var seller = _sellersService.FindById(id.Value);
+            if (seller == null)
+            {
+                return NotFound();
+            }
+
+            return View(seller);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Delete(int id)
+        {
+            _sellersService.Remove(id);
+            return RedirectToAction(nameof(Index));
+        }
     }
 }
